8d209b9463f7558c5d6a85a0fac5a7fd08aca98b
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-06-12 13:22:45 +0200

    implement makeBlack button

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('HI')
+ulList = document.querySelectorAll('ul li')
+
+// console.log(ulList)
+
+function makeBlack() {
+    for (let i = 0; i < ulList.length; i++) {
+        console.log(i)
+        ulList[i].style.color = "black"
+    }
+}
